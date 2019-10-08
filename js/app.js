c7df8ce47e4cd1492823b932daa53b14b0b09b85
Arjun Nair <nair.arun47@yahoo.com>
2019-10-08 09:31:18 -0700

    Random questions chosen from array

--- js/app.js
+++ js/app.js
@@ -453,25 +453,25 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
     function showQuestions(questions, quizContainer) {
         let output = [];
         let answers;
+        let random = Math.floor(5 * Math.random());
+
         
+        answers = [];
 
-        for (let i = 0; i < questions.length; i++){
-            answers = [];
-
-            for (letter in questions[i].answers) {
-                answers.push(
-                    '<label>'
-                    + '<input type="radio" name="question' + i + '" value="' + letter + '">'
-                    + letter + ': '
-                    + questions[i].answers[letter]
-                    + '</label>'
-                );
-            }
-            output.push(
-                '<div class="question">' + questions[i].question + '</div>'
-                + '<div class="answers">' + answers.join('') + '</div>'
+        for (letter in questions[random].answers) {
+            answers.push(
+                '<label>'
+                + '<input type="radio" name="question' + random + '" value="' + letter + '">'
+                + letter + ': '
+                + questions[random].answers[letter]
+                + '</label>'
             );
         }
+        output.push(
+            '<div class="question">' + questions[random].question + '</div>'
+            + '<div class="answers">' + answers.join('') + '</div>'
+        );
+        
 
         // code will go here
 
